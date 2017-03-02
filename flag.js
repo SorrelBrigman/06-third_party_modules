1edd0f273274a149ded6356d2c4d633e175d9e5e
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-02 15:59:06 -0600

    top star now a function

--- flag.js
+++ flag.js
@@ -8,11 +8,20 @@ let { bgRed: rStripe, bgWhite: wStripe } = chalk;
 let {bgBlue: stars} = chalk.white;
 
 
+var x = '\u03A9';
+
+let topStarString = ()=> {
+  let myString = " "
+  for(var i = 0; i < 7; i ++) {
+    myString += `${x} `;
+  }
+  return myString;
+}
 
 
 const topStripe = "                                   ";
-const topRowStars = stars(" * * * * * * * ");
-const otherRowStars = stars("  * * * * * *  ");
+const topRowStars = stars(topStarString());
+const otherRowStars = stars(`  ${x} ${x} ${x} ${x} ${x} ${x}  `);
 const lowerStripe = "                                                  ";
 const shortRed = rStripe(topStripe);
 const shortWhite = wStripe(topStripe);
