61309f50eaf6d0dcee0c332b21d84b5bad115102
Sorrel Brigman <sorrel.k@gmail.com>
2017-03-02 15:48:44 -0600

    uses es6 deconstruction to access colors

--- flag.js
+++ flag.js
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
 
 const chalk = require("chalk");
+
+//use es6 destructuing to access needed colors
+
+let { bgRed: rStripe, bgWhite: wStripe } = chalk;
+let {bgBlue: stars} = chalk.white;
+
+
+
+
 const topStripe = "                                   ";
-const topRowStars = chalk.white.bgBlue(" * * * * * * * ");
-const otherRowStars = chalk.white.bgBlue("  * * * * * *  ");
+const topRowStars = stars(" * * * * * * * ");
+const otherRowStars = stars("  * * * * * *  ");
 const lowerStripe = "                                                  ";
-const shortRed = chalk.bgRed(topStripe);
-const shortWhite = chalk.bgWhite(topStripe);
-const longRed = chalk.bgRed(lowerStripe);
-const longWhite = chalk.bgWhite(lowerStripe);
+const shortRed = rStripe(topStripe);
+const shortWhite = wStripe(topStripe);
+const longRed = rStripe(lowerStripe);
+const longWhite = wStripe(lowerStripe);
 
 const topRed =topRowStars + shortRed;
-const topWhite = topRowStars + shortWhite;
+const topWhite = otherRowStars + shortWhite;
 
 console.log(topRed);
 console.log(topWhite);
